cbf9cc156bb68f16e108334c2e1d63f6e0ff7061
timmtonga <timmtonga@gmail.com>
2015-04-18 12:57:21 +0200

    Improved code for pulling counts

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,19 +6,29 @@ class DashboardController < ApplicationController
  def get_counts
   scores = {}
 
+  children = Child.by_birth_district_and_record_status
+
+  (children || []).each do |child|
+   scores[child.birth_district] = Hash.new(0) if scores[child.birth_district].blank?
+   scores[child.birth_district][child.record_status] += 1
+  end
+
+=begin
   districts = District.by_name.all.collect{|x| [x.name, x.id]}
 
   (districts || []).each do |name, code|
 
    counts = Child.by_district_code.keys(["#{code}"])
-   if counts.rows.count > 0
-    scores[name] = Hash.new(0)
-    (counts || []).each do |child|
-     scores[name][child.record_status] +=1
-    end
-   end
-  end
 
+    if counts.rows.count > 0
+     scores[name] = Hash.new(0)
+     (counts || []).each do |child|
+      scores[name][child.record_status] +=1
+     end
+    end
+
+  end
+=end
   render :text => scores.to_json
  end
 end
