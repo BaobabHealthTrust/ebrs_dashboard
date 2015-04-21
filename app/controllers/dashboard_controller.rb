1e7d66ace67ed2eb9a8e7c777f43a8326a6b5b1c
timmtonga <timmtonga@gmail.com>
2015-04-21 12:43:51 +0200

    Changed statisctics from being queried directly with ajax to being read from file

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,35 +4,18 @@ class DashboardController < ApplicationController
  end
 
  def get_counts
+
+  file_name = Rails.root.join('docs', 'statistics.txt')
+  file = YAML.load_file(file_name)
   scores = {}
 
-  children = Child.by_birth_district_and_record_status
-  begin
-   (children || []).each do |child|
-    scores[child.birth_district] = Hash.new(0) if scores[child.birth_district].blank?
-    scores[child.birth_district][child.record_status] += 1
-   end
-  rescue RestClient::ResourceNotFound => nfe
-   scores
-  end
-
-
-=begin
-  districts = District.by_name.all.collect{|x| [x.name, x.id]}
-
-  (districts || []).each do |name, code|
-
-   counts = Child.by_district_code.keys(["#{code}"])
-
-    if counts.rows.count > 0
-     scores[name] = Hash.new(0)
-     (counts || []).each do |child|
-      scores[name][child.record_status] +=1
-     end
+  (file || []).each do |district, statistics|
+    scores[district]= {}
+    (statistics || []).each do |type, statistic|
+      scores[district][type] = statistic
     end
-
   end
-=end
+
   render :text => scores.to_json
  end
 end
